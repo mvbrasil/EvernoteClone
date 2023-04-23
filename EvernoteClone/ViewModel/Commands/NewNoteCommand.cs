3bfd8d9ac0bade7ae269b890fea095396c913c91
Marcos Brasil <marcosvbrasil@yahoo.com.br>
2023-04-23 20:55:30 -0300

    Creating views and preparing commands

---
+++ EvernoteClone/ViewModel/Commands/NewNoteCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+    public class NewNoteCommand : ICommand
+    {
+        public NotesVM VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public NewNoteCommand(NotesVM vm)
+        {
+            VM = vm;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO: Create new note
+        }
+    }
+}
