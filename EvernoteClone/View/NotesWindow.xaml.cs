d31eac6368770f568e397d05bd6d704a91127a38
Marcos Brasil <marcosvbrasil@yahoo.com.br>
2023-04-30 12:39:39 -0300

    implementing toggleBUtton for bold property

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -12,9 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Speech;
-using System.Speech.Recognition;
 using System.Threading;
 using System.Globalization;
+using System.Windows.Controls.Primitives;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace EvernoteClone.View
 {
@@ -23,34 +25,11 @@ namespace EvernoteClone.View
     /// </summary>
     public partial class NotesWindow : Window
     {
-        SpeechRecognitionEngine recognizer;
+        //SpeechRecognitionEngine recognizer;
         
         public NotesWindow()
         {
             InitializeComponent();
-
-            var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
-                                 where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
-                                 select r).FirstOrDefault();
-
-            recognizer = new SpeechRecognitionEngine(currentCulture);
-
-            //GrammarBuilder builder = new GrammarBuilder();
-            //builder.AppendDictation();
-            //Grammar grammar = new Grammar(builder);
-            //recognizer.LoadGrammar(grammar);
-
-            recognizer.LoadGrammar(new DictationGrammar());
-
-            recognizer.SetInputToDefaultAudioDevice();
-
-            recognizer.SpeechRecognized += Recognizer_SpeechReconized;
-        }
-
-        private void Recognizer_SpeechReconized(object sender, SpeechRecognizedEventArgs e)
-        {
-            string reconizedText = e.Result.Text;
-            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(reconizedText)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -66,33 +45,18 @@ namespace EvernoteClone.View
             // Utiliza o pacote NuGet Microsoft.CognitiveServices.Speech
 
             //-----------------------
-            //string region = "southcentralus";
-            //string key = "ca2d92581df5497591263a1470030839";
+            string region = "southcentralus";
+            string key = "ca2d92581df5497591263a1470030839";
 
-            //var speechConfig = SpeechConfig.FromSubscription(key, region);
+            var speechConfig = SpeechConfig.FromSubscription(key, region);
 
-            //using(var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
-            //{
-            //    using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
-            //    {
-            //        var result = await recognizer.RecognizeOnceAsync();
-            //        contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(result.Text)));
-            //    }
-            //}
-
-
-            // Speech Recognizer with .NET Framework
-            // Utiliza a referencia System.Speech
-
-            if (!isRecognizing)
+            using (var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
             {
-                recognizer.RecognizeAsync(RecognizeMode.Multiple);
-                isRecognizing = true;
-            }
-            else
-            {
-                recognizer.RecognizeAsyncStop();
-                isRecognizing = false;
+                using (var recognizer = new SpeechRecognizer(speechConfig, audioConfig))
+                {
+                    var result = await recognizer.RecognizeOnceAsync();
+                    contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(result.Text)));
+                }
             }
 
         }
@@ -108,8 +72,24 @@ namespace EvernoteClone.View
         {
             //var textToBold = new TextRange(contentRichTextBox.Selection.Start, contentRichTextBox.Selection.End);
 
-            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+            bool isButtonChecked = (sender as ToggleButton).IsChecked ?? false;
 
+            if(isButtonChecked)
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+            }
+            else
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Normal);
+            }
+            
+        }
+
+        private void contentRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var selectedweight = contentRichTextBox.Selection.GetPropertyValue(FontWeightProperty);
+
+            boldButton.IsChecked = (selectedweight != DependencyProperty.UnsetValue) && (selectedweight.Equals(FontWeights.Bold));
         }
     }
 }
