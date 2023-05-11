9c58d135a5436f417062cb6bfc1388aef290222c
Marcos Brasil <marcosvbrasil@yahoo.com.br>
2023-05-10 22:53:30 -0300

    Register users with firebase authentication

--- EvernoteClone/ViewModel/LoginVM.cs
+++ EvernoteClone/ViewModel/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using EvernoteClone.Model;
 using EvernoteClone.ViewModel.Commands;
+using EvernoteClone.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -179,9 +180,9 @@ namespace EvernoteClone.ViewModel
             // TODO: login
         }
 
-        public void Register()
+        public async void Register()
         {
-            // TODO: Register
+            await FirebaseAuthHelper.Register(User);
         }
 
         private void OnPropertyChanged(string propertyName)
